db83898d3477a86687b0dd37afa0cde69aa30e99
James-Short <143297015+James-Short@users.noreply.github.com>
2025-05-27 23:15:27 -0500

    Move main.cpp to folder

--- NumberGuess/main.cpp
+++ NumberGuess/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+
+    int num = (rand() % 100) + 1;
+    int guess;
+
+    cout << "Please guess a number 1 through 100: \n";
+    cin >> guess;
+
+    while(guess != num){
+        if(guess > num){
+            cout << "Your guess is higher than my number! \n";
+        }
+        else if(guess < num){
+            cout << "Your guess is lower than my number! \n";
+        }
+
+        cout << "Try again: ";
+        
+        cin >> guess;
+    }
+
+    cout << "Correct, my number is " << num << "!";
+    
+
+    
+
+    return 0;
+}
